0fc1cccc3f64625827c1196b1f04baaccfc6b0b8
iARS003 <akshayrajsaxena3@gmail.com>
2026-03-09 10:52:29 +0530

    First Commit

---
+++ src/com/git/Demo.java
@@ -0,0 +1,14 @@
+package com.git;
+
+public class Demo 
+{
+	public static void main(String[] args) 
+	{
+		int a = 4;
+		int b = 3;
+		System.out.println(a+b);
+		if (a+b==7)
+			System.out.println("Thala for a reason");
+	}
+
+}
