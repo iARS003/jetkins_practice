af532cfe9a0bc98915c80134121a0dc51293c173
iARS003 <akshayrajsaxena3@gmail.com>
2026-03-09 12:08:10 +0530

    New Update

--- src/com/git/Demo.java
+++ src/com/git/Demo.java
@@ -5,10 +5,12 @@ public class Demo
 	public static void main(String[] args) 
 	{
 		int a = 4;
-		int b = 3;
+		int b = 10;
 		System.out.println(a+b);
 		if (a+b==7)
 			System.out.println("Thala for a reason");
+		else if ((a+b)%7>1)
+			System.out.println("Multiple Thala for a reason");
 	}
 
 }
